086ffee00af380dc4702d03b8e3eb6dd5d511ac6
Angelica Bufano <abufano@tql.com>
2024-06-14 13:31:33 -0400

    Add project files.

---
+++ SecretNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SecretNumber;
+
+internal class Program {
+    static void Main(string[] args) {
+
+        TQL tql = new TQL();
+        tql.SecretNumber = 1;
+        tql.DisplayClassname();
+        var newSecretNumber = tql.RandomizeSecretNumber(321);
+        Console.WriteLine($"newSecretNumber is {newSecretNumber}");
+        tql.DisplayClassname();
+    }
+}
+
+internal class TQL {
+    public int SecretNumber { get; set; }
+
+    public void DisplayClassname() {
+        Console.WriteLine($"Classname is TQL; SecretNumber is {SecretNumber}");
+    }
+    public int RandomizeSecretNumber(int RandomNumber) {
+        SecretNumber *= RandomNumber;
+        return SecretNumber;
+    }
+}
